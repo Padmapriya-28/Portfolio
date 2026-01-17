6cb45fee3cabae0a9a68cb5bc51f97122f20442b
Padmapriya Saravanan <padmapriyasaravanan28@gmail.com>
2026-01-17 13:48:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Data Science Enthusiast", "Python Developer", "ML Aspirant", "Problem Solver", "Prompt Engineer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 2000,
+    loop: true
+})
